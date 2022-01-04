5c5667aa4cee1ac46d3b5d7f7716691aaf29453f
piyoraik <42093052+piyoraik@users.noreply.github.com>
2022-01-05 01:59:16 +0900

    23. implementing a Service

---
+++ src/messages/message.service.ts
@@ -0,0 +1,21 @@
+import { MessagesRepository } from './message.repository';
+
+export class MessageService {
+  messageRepo: MessagesRepository;
+
+  constructor() {
+    this.messageRepo = new MessagesRepository();
+  }
+
+  findOne(id: string) {
+    return this.messageRepo.findOne(id);
+  }
+
+  findAll() {
+    return this.messageRepo.findAll();
+  }
+
+  create(content: string) {
+    return this.messageRepo.create(content);
+  }
+}
