84ac0f168088cda44cf4488d9c1d92226b303a8c
piyoraik <42093052+piyoraik@users.noreply.github.com>
2022-01-05 02:07:44 +0900

    24. Manual Testing of the Controller

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,19 +1,27 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessageService } from './message.service';
 
 @Controller('messages')
 export class MessagesController {
+  messageService: MessageService;
+
+  constructor() {
+    this.messageService = new MessageService();
+  }
+
   @Get()
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
-  listMessages() {}
+  listMessages() {
+    return this.messageService.findAll();
+  }
 
   @Post()
   createMessage(@Body() body: CreateMessageDto) {
-    console.log(body);
+    return this.messageService.create(body.content);
   }
 
   @Get('/:id')
   getMessage(@Param('id') id: string) {
-    console.log(id);
+    return this.messageService.findOne(id);
   }
 }
